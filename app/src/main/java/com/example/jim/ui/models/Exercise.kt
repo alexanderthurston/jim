5e1355f910a494bb1e72801956137c148bc6edf0
Alexander Thurston <alexanderthurston@gmail.com>
2022-11-22 21:58:04 -0700

    Added models

---
+++ app/src/main/java/com/example/jim/ui/models/Exercise.kt
@@ -0,0 +1,7 @@
+package com.example.jim.ui.models
+
+data class Exercise(
+    val userId: String? = null,
+    val id: String? = null,
+    val name: String? = null,
+)
---
+++ app/src/main/java/com/example/jim/ui/models/Set.kt
@@ -0,0 +1,10 @@
+package com.example.jim.ui.models
+
+data class Set(
+    val userId: String? = null,
+    val id: String? = null,
+    val reps: Int? = null,
+    val weight: Float? = null,
+    val exerciseId: String? = null,
+    val workoutId: String? = null,
+)
